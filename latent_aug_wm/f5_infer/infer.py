major bug fix (gradient pass through vocoder), added forward backward experiment

latent_aug_wm/f5_infer/infer.py
@@ -51,6 +51,7 @@ class F5TTSBatchInferencer(F5TTS):
 
         for param in self.vocoder.parameters():
             param.requires_grad = False
+
         if train:
             self.vocoder = self.vocoder.half()
 
@@ -109,7 +110,7 @@ class F5TTSBatchInferencer(F5TTS):
         self.ema_model.transformer.clear_cache()
         assert self.ema_model.transformer.text_cond is None
         generated_rebatched = rebatching_from_varying_length(generated, lens, duration)
-        gr_wave = self.vocoder.decode(generated_rebatched.permute(0, 2, 1))
+        gr_wave = self.vocoder.decode_with_grad(generated_rebatched.permute(0, 2, 1))
 
         result = {
             "generated_rebatched_mel": generated_rebatched,
@@ -118,7 +119,7 @@ class F5TTSBatchInferencer(F5TTS):
         if not eval:
             return result
         with torch.no_grad():
-            result["g_wave"] = self.vocoder.decode(generated.permute(0, 2, 1))
+            result["g_wave"] = self.vocoder.decode_with_grad(generated.permute(0, 2, 1))
         return result
 
 

latent_aug_wm/logging_utils/metrics.py
@@ -29,31 +29,43 @@ class BinaryF1Metric(BaseMetric):
 
     def __init__(self, *args, **kwargs):
         self.metric = BinaryF1Score()
+        self.start_update = False
 
     def step_process(self, batch):
 
-        real_detector_logits = batch["real_detector_logits"]
-        fake_detector_logits = batch["fake_detector_logits"]
+        real_detector_logits = batch.get("real_detector_logits", [])
+        fake_detector_logits = batch.get("fake_detector_logits", [])
 
-        real_detector_pred = torch.argmax(real_detector_logits, dim=-1).detach().cpu()
-        fake_detector_pred = torch.argmax(fake_detector_logits, dim=-1).detach().cpu()
-        full_pred_labels = (
-            torch.cat((real_detector_pred, fake_detector_pred)).detach().cpu()
-        )
-        labels = torch.zeros(
-            real_detector_logits.shape[0] + fake_detector_logits.shape[0],
-            dtype=torch.long,
-        )
-        labels[: real_detector_logits.shape[0]] = 1
-        self.metric.update(full_pred_labels, labels)
+        if real_detector_logits:
+            self.start_update = True
+            real_detector_pred = (
+                torch.argmax(real_detector_logits, dim=-1).detach().cpu()
+            )
+            fake_detector_pred = (
+                torch.argmax(fake_detector_logits, dim=-1).detach().cpu()
+            )
+            full_pred_labels = (
+                torch.cat((real_detector_pred, fake_detector_pred)).detach().cpu()
+            )
+            labels = torch.zeros(
+                real_detector_logits.shape[0] + fake_detector_logits.shape[0],
+                dtype=torch.long,
+            )
+            labels[: real_detector_logits.shape[0]] = 1
+            self.metric.update(full_pred_labels, labels)
 
     def reset(self):
+        self.start_update = False
         self.metric.reset()
 
     def __call__(self, writer, epoch):
-        f1_score = float(self.metric.compute().detach().cpu().numpy())
+        if self.start_update:
+            f1_score = float(self.metric.compute().detach().cpu().numpy())
+        else:
+            f1_score = 0.0
 
         writer.add_scalar("eval/f1", f1_score, epoch)
+
         self.reset()
         return {"f1": f1_score}
 
@@ -61,9 +73,13 @@ class BinaryF1Metric(BaseMetric):
 class BinaryPrecisionMetric(BinaryF1Metric):
     def __init__(self, *args, **kwargs):
         self.metric = MulticlassPrecision(num_classes=2)
+        self.start_update = False
 
     def __call__(self, writer, epoch):
-        precision_score = float(self.metric.compute().detach().cpu().numpy())
+        if self.start_update:
+            precision_score = float(self.metric.compute().detach().cpu().numpy())
+        else:
+            precision_score = 0.0
 
         writer.add_scalar("eval/precision", precision_score, epoch)
         self.reset()

latent_aug_wm/loss/base.py
@@ -25,9 +25,7 @@ def generate_multiple_mel_from_f5tts(nets=None, step=None, eval=False, **kwargs)
     orig_noise = nets.noise_encoder.get_non_wm_latent(random_noise)
 
     # with torch.no_grad():
-    wm_out = nets.f5tts(
-        fix_noise=wm_noise, use_grad_checkpoint=(not eval), eval=eval, **kwargs
-    )
+
     with torch.no_grad():
         orig_out = nets.f5tts(
             fix_noise=orig_noise, use_grad_checkpoint=False, eval=eval, **kwargs
@@ -35,6 +33,9 @@ def generate_multiple_mel_from_f5tts(nets=None, step=None, eval=False, **kwargs)
         out = nets.f5tts(
             fix_noise=random_noise, use_grad_checkpoint=False, eval=eval, **kwargs
         )
+    wm_out = nets.f5tts(
+        fix_noise=wm_noise, use_grad_checkpoint=(not eval), eval=eval, **kwargs
+    )
     # out:
     # {
     #     "generated_rebatched": generated_rebatched,
@@ -62,7 +63,7 @@ def mel_reg_L1_loss(nets=None, step=None, scale=None, **kwargs):
     return {"mel_reg_l1_loss": mel_reg_loss}, []
 
 
-def detector_loss(nets=None, step=None, scale=None, **kwargs):
+def detector_loss(nets=None, step=None, scale=1.0, **kwargs):
     real_input, fake_input = kwargs["rand_gr_wave"], kwargs["wm_gr_wave"]
     batch_size, seq_len = real_input.shape
 
@@ -75,9 +76,10 @@ def detector_loss(nets=None, step=None, scale=None, **kwargs):
     fake_detector_logits, fake_loss = nets.detector.calculate_loss(
         fake_input.unsqueeze(1), fake_logits
     )
+    print(scale)
 
     return {
-        "detector_loss": real_loss + fake_loss,
+        "detector_loss": (real_loss + fake_loss) * scale,
         "real_detector_logits": real_detector_logits.detach().cpu(),
         "fake_detector_logits": fake_detector_logits.detach().cpu(),
     }, ["detector"]
@@ -104,7 +106,8 @@ if __name__ == "__main__":
     from latent_aug_wm.model.decoder import BaseAudioSealClassifier
     from latent_aug_wm.f5_infer.infer import F5TTSBatchInferencer
 
-    common_latent = torch.randn((2000, 100))
+    # common_latent = torch.randn((2000, 100))
+    common_latent = torch.randn((100, 2000))
 
     nets = AttrMap(
         {
@@ -170,8 +173,8 @@ if __name__ == "__main__":
     loss_fn = construct_loss_fn(
         [
             generate_multiple_mel_from_f5tts,
-            mel_reg_L1_loss,
-            AugApplier(aug_obj),
+            # mel_reg_L1_loss,
+            # AugApplier(aug_obj),
             detector_loss,
         ],
         log_fn_time=False,
@@ -181,12 +184,12 @@ if __name__ == "__main__":
     nets.f5tts.ema_model = nets.f5tts.ema_model.half()
 
     with torch.cuda.amp.autocast():
-        _ = loss_fn(nets, {}, batch, 0, eval=False)
-        del _
-        total_loss, loss_items, _, items = loss_fn(nets, {}, batch, 0, eval=True)
+        total_loss, loss_items, _ = loss_fn(nets, {}, batch, 0, eval=False)
+        # del _
+        # total_loss, loss_items, _, items = loss_fn(nets, {}, batch, 0, eval=True)
 
     total_loss.backward()
-    print(items.keys())
+    # print(items.keys())
     print("loss", loss_items)
     for n, p in nets.noise_encoder.named_parameters():
         if p.requires_grad:

latent_aug_wm/loss/f5_latent_aug.py
@@ -10,6 +10,7 @@ import torch.nn.functional as F
 from latent_aug_wm.f5_infer.forward_backward import sample_from_model
 
 l1_loss = nn.L1Loss()
+l2_loss = nn.MSELoss()
 
 
 class F5TTSForwardBackwardNoiseAug:
@@ -28,7 +29,22 @@ class F5TTSForwardBackwardNoiseAug:
             forward_backward_step=self.forward_backward_step,
             **kwargs,
         )
-        return {"orig_noise": random_noise, "aug_noise": aug_noise}, ["noise_encoder"]
+        # with torch.no_grad():
+        #     orig_inv_noise = sample_from_model(
+        #         fix_noise=random_noise,
+        #         model=nets.f5tts.ema_model,
+        #         inference_kwargs=self.inference_kwargs,
+        #         forward_backward_step=self.forward_backward_step,
+        #         **kwargs,
+        #     )
+        return (
+            {
+                "orig_noise": random_noise,
+                "aug_noise": aug_noise,
+                # "orig_inv_noise": orig_inv_noise
+            },
+            ["noise_encoder"],
+        )
 
 
 def len_to_mask(lens, max_dur):
@@ -41,19 +57,44 @@ def len_to_mask(lens, max_dur):
 
 
 def noise_reg_L1_loss(nets=None, step=None, scale=None, **kwargs):
+    # orig_noise = kwargs["orig_inv_noise"]
     orig_noise = kwargs["orig_noise"]
     aug_noise = kwargs["aug_noise"]
 
-    _, seq_len, num_channel = orig_noise.shape
-    assert num_channel == 100
+    # _, seq_len, num_channel = orig_noise.shape
+    # assert num_channel == 100
 
-    lens = kwargs["lens"]
-    mask = len_to_mask(lens, seq_len)
-    noise_reg_loss = torch.abs(
-        (orig_noise - aug_noise) * mask.unsqueeze(-1)
-    ).sum() / torch.sum(mask)
+    # lens = kwargs["lens"]
+    # mask = len_to_mask(lens, seq_len)
+    # noise_reg_loss = torch.abs(
+    #     (orig_noise - aug_noise) * mask.unsqueeze(-1)
+    # ).sum() / torch.sum(mask)
 
     # noise_reg_loss = l1_loss(aug_noise, orig_noise)
+
+    noise_reg_loss = l1_loss(aug_noise, orig_noise)
+    if scale is not None:
+        noise_reg_loss = noise_reg_loss * scale
+    return {"noise_reg_loss": noise_reg_loss}, []
+
+
+def noise_reg_L2_loss(nets=None, step=None, scale=None, **kwargs):
+    # orig_noise = kwargs["orig_inv_noise"]
+    orig_noise = kwargs["orig_noise"]
+    aug_noise = kwargs["aug_noise"]
+
+    # _, seq_len, num_channel = orig_noise.shape
+    # assert num_channel == 100
+
+    # lens = kwargs["lens"]
+    # mask = len_to_mask(lens, seq_len)
+    # noise_reg_loss = torch.abs(
+    #     (orig_noise - aug_noise) * mask.unsqueeze(-1)
+    # ).sum() / torch.sum(mask)
+
+    # noise_reg_loss = l1_loss(aug_noise, orig_noise)
+
+    noise_reg_loss = l2_loss(aug_noise, orig_noise)
     if scale is not None:
         noise_reg_loss = noise_reg_loss * scale
     return {"noise_reg_loss": noise_reg_loss}, []
@@ -84,10 +125,10 @@ def generate_multiple_mel_from_f5tts_with_aug_noise(
     wm_out = {("wm_" + k): v for k, v in wm_out.items()}
     orig_out = {("rand_" + k): v for k, v in orig_out.items()}
 
-    wm_out.update(out)
+    # wm_out.update(out)
     wm_out.update(orig_out)
 
-    return wm_out, ["noise_encoder"]
+    return wm_out, []
 
 
 if __name__ == "__main__":

latent_aug_wm/loss/mel_gen.py
@@ -9,13 +9,14 @@ import torch.nn.functional as F
 
 from latent_aug_wm.model.encoder import UniqueNoiseEncoder, UniqueNoiseEncoderRemoveLen
 from latent_aug_wm.data_augmentation.base import BaseBatchAugmentation
+from latent_aug_wm.loss.base import detector_loss
 from loguru import logger as glogger
 
 
 def generate_multiple_mel_from_f5tts_with_lens(
     nets=None, step=None, eval=False, **kwargs
 ):
-    # ONLY works with model.encoder.UniqueNoiseEncoder
+    # ONLY works with model.encoder.UniqueNoiseEncoderRemoveLen
     assert isinstance(nets.noise_encoder, UniqueNoiseEncoderRemoveLen)
     common_latent = nets.noise_encoder.common_latent
     lens = kwargs["lens"]
@@ -26,10 +27,14 @@ def generate_multiple_mel_from_f5tts_with_lens(
     orig_noise = nets.noise_encoder.get_non_wm_latent(random_noise, lens)
 
     # with torch.no_grad():
-    wm_out = nets.f5tts(fix_noise=wm_noise, use_grad_checkpoint=(not eval), **kwargs)
     with torch.no_grad():
         orig_out = nets.f5tts(fix_noise=orig_noise, **kwargs)
         out = nets.f5tts(fix_noise=random_noise, **kwargs)
+
+    # wm_out = nets.f5tts(fix_noise=wm_noise, use_grad_checkpoint=(not eval), eval=eval,**kwargs)
+    wm_out = nets.f5tts(
+        fix_noise=wm_noise, use_grad_checkpoint=True, eval=eval, **kwargs
+    )
     # out:
     # {
     #     "generated_rebatched": generated_rebatched,
@@ -42,5 +47,98 @@ def generate_multiple_mel_from_f5tts_with_lens(
 
     wm_out.update(out)
     wm_out.update(orig_out)
+    # assert(nets.noise_encoder.special_latent.requires_grad)
+    # print("special_latent grad:", model.special_latent.grad)
 
     return wm_out, ["noise_encoder"]
+
+
+if __name__ == "__main__":
+    from attrmap import AttrMap
+    from latent_aug_wm.model.decoder import BaseAudioSealClassifier
+    from latent_aug_wm.f5_infer.infer import F5TTSBatchInferencer
+
+    # common_latent = torch.randn((2000, 100))
+    common_latent = torch.randn((100, 2000))
+
+    nets = AttrMap(
+        {
+            "noise_encoder": UniqueNoiseEncoderRemoveLen(common_latent).cuda(),
+            # "f5tts": F5TTSBatchInferencer(model="F5TTS_Small", device="cuda", train=True),
+            "f5tts": F5TTSBatchInferencer(device="cuda", train=True),
+            "detector": BaseAudioSealClassifier(input_dim=1).cuda(),
+        }
+    )
+    # nets.f5tts.ema_model = nets.f5tts.ema_model.half()
+
+    glogger.debug("created models")
+    from latent_aug_wm.dataset.mel_dataset import get_combine_dataloader
+
+    ref_wav_file = "/home/tst000/projects/datasets/selected_ref_files.txt"
+    gen_txt_fname = "/home/tst000/projects/datasets/selected_gen_text.txt"
+
+    mel_spec_kwargs = {
+        "target_sample_rate": 24000,
+        "n_mel_channels": 100,
+        "hop_length": 256,
+        "win_length": 1024,
+        "n_fft": 1024,
+        "mel_spec_type": "vocos",
+    }
+    tmp_dir = "/home/tst000/projects/tmp/libriTTS"
+
+    data_iter = get_combine_dataloader(
+        ref_wav_file=ref_wav_file,
+        gen_txt_fname=gen_txt_fname,
+        mel_spec_kwargs=mel_spec_kwargs,
+        tmp_dir="/home/tst000/projects/tmp/libriTTS",
+        shuffle=False,
+        unsorted_batch_size=256,
+        batch_size=1,
+        allowed_padding=50,
+    )
+    glogger.debug("completed batching")
+    batch = next(data_iter)
+    if isinstance(batch, list):
+        batch = [(b.to("cuda") if torch.is_tensor(b) else b) for b in batch]
+    else:
+        batch = {
+            k: (v.to("cuda") if torch.is_tensor(v) else v) for k, v in batch.items()
+        }
+
+    from latent_aug_wm.loss import construct_loss_fn
+
+    transform_configs = {
+        "AddColoredNoise": {
+            "mode": "per_example",
+            "p": 1.0,
+            "min_snr_in_db": 10.0,
+            "max_snr_in_db": 10.0,
+        }
+    }
+    # scaler = torch.cuda.amp.GradScaler()
+
+    glogger.debug("completed aug obj")
+    loss_fn = construct_loss_fn(
+        [
+            generate_multiple_mel_from_f5tts_with_lens,
+            detector_loss,
+        ],
+        log_fn_time=False,
+    )
+    glogger.debug("completed loss func")
+
+    # nets.f5tts.ema_model = nets.f5tts.ema_model.half()
+
+    # with torch.cuda.amp.autocast():
+    with torch.autocast(device_type="cuda", dtype=torch.bfloat16):
+        total_loss, loss_items, _ = loss_fn(nets, {}, batch, 0, eval=False)
+        # del _
+        # total_loss, loss_items, _, items = loss_fn(nets, {}, batch, 0, eval=True)
+
+    total_loss.backward()
+    # print(items.keys())
+    print("loss", loss_items)
+    for n, p in nets.noise_encoder.named_parameters():
+        if p.requires_grad:
+            print(n, p.grad.abs().mean())

latent_aug_wm/model/encoder.py
@@ -10,7 +10,7 @@ import torch.nn as nn
 # from torch.nn.utils.parametrizations import weight_norm
 from torch import linalg as LA
 
-# from latent_aug_wm.f5_infer.forward_backward import load_peft_f5tts_linear, sample_from_model
+from latent_aug_wm.loss.f5_latent_aug import len_to_mask
 
 
 class BasicEncoder(nn.Module):
@@ -109,7 +109,7 @@ class UniqueNoiseEncoderRemoveLen(nn.Module):
         self.common_latent = common_latent[:, : self.max_length].permute(1, 0)
         self.max_weight_norm = max_weight_norm
         self.special_latent = torch.nn.Parameter(
-            torch.zeros(self.max_length, self.num_channel),
+            torch.randn(self.max_length, self.num_channel) * 0.01,
             requires_grad=True,
         )
         assert self.common_latent.shape == self.special_latent.shape
@@ -117,6 +117,9 @@ class UniqueNoiseEncoderRemoveLen(nn.Module):
     def forward(self, x, lens):
         batch_size, seq_length, num_channel = x.shape
         common_latent = self.common_latent.to(x.device).type(x.dtype)
+
+        mask = len_to_mask(lens, seq_length)
+        inv_mask = (mask - 1) * -1
 
         assert num_channel == self.num_channel
         assert seq_length <= self.max_length
@@ -128,15 +131,20 @@ class UniqueNoiseEncoderRemoveLen(nn.Module):
         if weight_norm > self.max_weight_norm:
             special_latent = self.max_weight_norm * special_latent / weight_norm
 
-        current_noise = (
+        current_masked_noise = (
             special_latent + common_latent  # .unsqueeze(0).repeat(batch_size, 1, 1)
-        )
+        )[:seq_length].unsqueeze(0) * mask.unsqueeze(-1)
 
+        masked_input = inv_mask.unsqueeze(-1) * x
+
+        # output = x.clone()
+        output = masked_input + current_masked_noise
         # current_noise = current_noise[:, :seq_length, :]
-        for batch_id, length in enumerate(lens):
-            current_seq_length = seq_length - length
-            x[batch_id, length:] = current_noise[0, :current_seq_length]
-        return x
+        # for batch_id, length in enumerate(lens):
+        #     current_seq_length = seq_length - length
+        #     output[batch_id, length:] = current_noise[:current_seq_length]
+
+        return output
 
     def get_non_wm_latent(self, x, lens):
         # created for contraining generation range
@@ -146,18 +154,20 @@ class UniqueNoiseEncoderRemoveLen(nn.Module):
         assert num_channel == self.num_channel
         assert seq_length <= self.max_length
         # current_noise = torch.stack([torch.clone(self.special_latent) for _ in range(batch_size)])
-        current_noise = common_latent.unsqueeze(0)  # .repeat(batch_size, 1, 1)
+        current_noise = common_latent  # .repeat(batch_size, 1, 1)
 
-        current_noise = current_noise[:, :seq_length, :]
+        current_noise = current_noise[:seq_length, :]
+        output = x.clone()
+
         for batch_id, length in enumerate(lens):
             current_seq_length = seq_length - length
-            x[batch_id, length:] = current_noise[0, :current_seq_length]
+            output[batch_id, length:] = current_noise[:current_seq_length]
         return x
 
 
 if __name__ == "__main__":
     common_latent = torch.randn((2000, 100))
-    noise_update_fn = UniqueNoiseEncoder(common_latent).half().cuda()
+    noise_update_fn = UniqueNoiseEncoderRemoveLen(common_latent).half().cuda()
 
     from latent_aug_wm.dataset.mel_dataset import get_combine_dataloader
 
@@ -198,6 +208,7 @@ if __name__ == "__main__":
         texts=batch["texts"],
         durations=batch["durations"].cuda(),
         lens=batch["lens"].cuda(),
+        use_grad_checkpoint=True,
     )
     for k, v in generated_dict.items():
         print(k, v.shape)

latent_aug_wm/trainer/base.py
@@ -9,6 +9,7 @@ import yaml
 import shutil
 import click
 import warnings
+import traceback
 
 
 import time
@@ -234,6 +235,18 @@ class Trainer(object):
         # for param, param_test in zip(model.parameters(), model_test.parameters()):
         #     param_test.data = torch.lerp(param.data, param_test.data, beta)
 
+    def get_grad_norm(self, model_name):
+        grads = [
+            param.grad.detach().flatten()
+            for param in self.model[model_name].parameters()
+            if param.grad is not None
+        ]
+        try:
+            norm = torch.cat(grads).norm()
+        except:
+            norm = 0
+        return norm
+
     def batch_detach(self, batch):
         if isinstance(batch, list) or isinstance(batch, tuple):
             batch = [v.detach() if torch.is_tensor(v) else v for v in batch]
@@ -270,15 +283,15 @@ class Trainer(object):
                 self.model, self.args.loss_weights, batch, eval=False
             )
         loss.backward()
-
         # for n, p in self.model.noise_encoder.named_parameters():
         #     if p.requires_grad:
         #         print(n, p.grad.abs().mean())
-        del loss
 
         for m in used_modules:
             # self.optimizer.step(m, scaler=scaler)
             self.optimizer.step(m)
+            model_norm = self.get_grad_norm(m)
+            self.writer.add_scalar("train/%s_grad_norm" % m, model_norm, self.steps)
 
         batch = self.batch_detach(batch)
         if (
@@ -309,7 +322,14 @@ class Trainer(object):
         ):
 
             gc.collect()
-            loss_items = self._train_step(batch)  # , scaler)
+            try:
+                loss_items = self._train_step(batch)  # , scaler)
+            except Exception as e:
+                traceback.print_exc()
+                batch = None
+                gc.collect()
+                torch.cuda.empty_cache()
+                continue
 
             for key in loss_items:
                 train_losses["train/%s" % key].append(loss_items[key])